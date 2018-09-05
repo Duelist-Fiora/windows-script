2e4a394286f2614dd5e24d1e21693b5081b9eca7
liangjh <liangjh@iaskdoc.com>
2018-09-05 11:25:18 +0800

    feat:配置多个项目根路径可供选择

--- pull-code.ps1
+++ pull-code.ps1
@@ -1,5 +1,17 @@
 $sourcePath=pwd
-$targetPath='C:\Users\l\1zhenghao'
+$pathList='C:\Users\l\1zhenghao','C:\Users\l\renbo'
+echo 'please select project, example: 0'
+for ($i = 0; $i -lt $pathList.Length; $i++) {
+    $selectPath=$pathList[$i]
+    echo $i')'$selectPath
+}
+$input = Read-Host
+while ($input -ge $pathList.Length)
+{
+    echo 'input error, please input again...'
+    $input = Read-Host
+}
+$targetPath=$pathList[$input]
 $dir=dir $targetPath
 echo 'pulling code...'
 for ($i = 0; $i -lt $dir.Count; $i++) {
